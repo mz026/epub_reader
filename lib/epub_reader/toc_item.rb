5df47493fbe4537de17e7539c14dc716ab90fbe9
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2014-05-29 18:24:23 +0800

    add TOCItem

--- lib/epub_reader/toc_item.rb
+++ lib/epub_reader/toc_item.rb
@@ -1,3 +1,33 @@
+require 'nokogiri'
 class EpubReader::TOCItem
+  class TOCItemException < Exception; end
+  class PathNotExistsException < TOCItemException; end
 
+  class << self
+    def create_from unzipped_path
+      raise PathNotExistsException unless Dir.exists?(unzipped_path)
+
+      ncx_path = Dir["#{unzipped_path}/**/*.ncx"].first
+      ncx_dir = Pathname.new(File.expand_path(File.dirname(ncx_path)))
+      
+      root = Nokogiri.XML(File.open(ncx_path)).root
+
+      nav_points = root.xpath('//xmlns:navPoint').map do |n|
+        relative_path = n.xpath('.//xmlns:content').first['src'].gsub(/#.*$/, '')
+
+        new({
+          :title => n.xpath('.//xmlns:text').text,
+          :path => ncx_dir.join(relative_path)
+        })
+      end
+    end
+
+  end
+
+
+  attr_reader :title, :path
+  def initialize options
+    @title = options[:title]
+    @path = options[:path]
+  end
 end
---
+++ spec/toc_item_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+describe EpubReader::TOCItem do
+  describe 'create_from(unzipped_path)' do
+    subject { EpubReader::TOCItem }
+    let(:unzipped_path) do
+      current_path = File.expand_path(File.dirname(__FILE__)) 
+      Pathname.new(current_path).join('fixtures', 'epub20_unzipped')
+    end
+    let(:ncx_path) do
+      ncx = Dir["#{unzipped_path}/**/*.ncx"].first
+      ncx_dirname = File.expand_path(File.dirname(ncx))
+      Pathname.new(ncx_dirname)
+    end
+
+    it 'raises if dir not exists' do
+      expect {
+        subject.create_from('non-exists')
+      }.to raise_error(subject::PathNotExistsException)
+    end
+
+    it 'returns items in toc.ncx' do
+      toc_items = subject.create_from(unzipped_path)
+      expected = [
+        {
+          :title => "I. THE BURIAL OF THE DEAD",
+          :path => ncx_path.join("wasteland-content.xhtml")
+        },
+        {
+          :title => "II. A GAME OF CHESS",
+          :path => ncx_path.join("wasteland-content.xhtml")
+        },
+        {
+          :title => "III. THE FIRE SERMON",
+          :path => ncx_path.join("wasteland-content.xhtml")
+        },
+        {
+          :title => "IV. DEATH BY WATER",
+          :path => ncx_path.join("wasteland-content.xhtml")
+        },
+        {
+          :title => "V. WHAT THE THUNDER SAID",
+          :path => ncx_path.join("wasteland-content.xhtml")
+        },
+        {
+          :title => 'NOTES ON "THE WASTE LAND"',
+          :path => ncx_path.join("wasteland-content.xhtml")
+        },
+      ]
+
+      toc_items.map do |i|
+        { :title => i.title, :path => i.path }
+      end.should == expected
+    end
+  end
+end
