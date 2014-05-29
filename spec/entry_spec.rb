2ef36221222ec77f4061f834ba5d24aa3a948069
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2014-05-29 18:39:02 +0800

    add Entry skeleton

---
+++ spec/entry_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe EpubReader::Entry do
+  describe '::new(toc_item)' do
+    pending
+  end
+
+  describe '#title' do
+    pending
+  end
+
+  describe '#html_text' do
+    pending
+  end
+
+  describe '#images' do
+    pending
+  end
+end
