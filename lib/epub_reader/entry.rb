538f0f11c9f44fcaad821f3d16bad911c6cf0cc0
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2014-05-30 16:49:04 +0800

    add Entry::new

--- lib/epub_reader/entry.rb
+++ lib/epub_reader/entry.rb
@@ -1,3 +1,12 @@
 class EpubReader::Entry
+  class EntryException < Exception; end
+  class PathNotExistsException < EntryException; end
 
+  def initialize toc_item
+    unless File.exists?(toc_item.path)
+      raise PathNotExistsException, "file #{toc_item.path} not exists" 
+    end
+
+
+  end
 end
--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -1,8 +1,30 @@
 require 'spec_helper'
 
 describe EpubReader::Entry do
+  subject { EpubReader::Entry }
+  let(:title) { 'the-entry-title' }
+  let(:fixture_path) do
+    Pathname.new(File.expand_path(__FILE__)).join('..', 'fixtures')
+  end
+
+  let(:toc_item) do 
+    path = fixture_path.join('epub20_unzipped', 
+                             'EPUB', 
+                             'wasteland-content.xhtml')
+    double(:title => title, :path => path)
+  end
+
   describe '::new(toc_item)' do
-    pending
+    it 'takes a toc item to init' do
+      entry = subject.new(toc_item)
+    end
+
+    it 'raises if `toc_item` path not exists' do
+      toc_item.stub(:path => Pathname.new('non-exists'))
+      expect {
+        entry = subject.new(toc_item)
+      }.to raise_error(EpubReader::Entry::PathNotExistsException)
+    end
   end
 
   describe '#title' do
